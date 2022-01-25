87ed313868a8f48b5a868c86132423a914d9e51e
gimume <yum17201619@gmail.com>
2022-01-25 15:02:38 +0900

    create Home layout

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from "../components/layout";
+// import sampleImage from '../public/img/sample-banner.jpg';
+import Image from 'next/image'
+import styles from '../styles/Home.module.css';
+import HomeLectureList from "../components/HomeLectureList";
 
 export default function Home() {
     return (
@@ -8,7 +12,24 @@ export default function Home() {
                 <title>{siteTitle}</title>
             </Head>
             <section>
-                <h1>Home</h1>
+                <div className={styles.banner}>
+                   <Image src="/img/sample-banner.jpg" alt="배너" width={804} height={240} />
+                    <div>
+                        <h1> 로그인 컴포넌트!</h1>
+                    </div>
+                </div>
+                {/*여기는 공지사항, ... 네비게이션 영역*/}
+                <nav className={styles.subNav}>
+                    <ul>
+                        <li><a>공지사항</a></li>
+                        <li><a>소개 페이지</a></li>
+                        <li><a>사용 방법</a></li>
+                    </ul>
+                </nav>
+                <div>
+                    <h1>강의 목록</h1>
+                    <HomeLectureList/>
+                </div>
             </section>
         </Layout>
     )
