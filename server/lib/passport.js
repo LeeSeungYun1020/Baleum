caeb1de4173296d77047c0bacc20b98baf6ab621
Lee Seung Yun <ileilliat@gmail.com>
2022-01-31 21:28:12 +0900

    [server] Add passport module

---
+++ server/lib/passport.js
@@ -0,0 +1,50 @@
+module.exports = function (app, mysql) {
+    const passport = require('passport')
+    const LocalStrategy = require('passport-local').Strategy
+    const bcrypt = require('bcrypt')
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+// 로그인 성공
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+// 이후 페이지 방문
+    passport.deserializeUser(function (id, done) {
+        mysql.query(
+            "SELECT * from user WHERE id=?",
+            [id],
+            function (error, results, fields) {
+                done(error, results[0])
+            })
+
+    });
+
+    passport.use(new LocalStrategy({
+            usernameField: 'id',
+            passwordField: 'pw'
+        },
+        function (username, password, done) {
+            mysql.query(
+                "SELECT * from user WHERE id=?",
+                [username],
+                function (err, result) {
+                    const user = result[0]
+                    if (err || result.length === 0)
+                        return done(null, false, {message: "username"})
+                    else if (password === user.pw) {
+                        return done(null, user)
+                    } else {
+                        bcrypt.compare(password, user.pw, function (err, result) {
+                            if (result === true) {
+                                return done(null, user)
+                            } else {
+                                return done(null, false, {message: "password"})
+                            }
+                        })
+                    }
+                })
+        }
+    ))
+    return passport
+}
