ea229207b15d458cdbeccdb8462532dba3a57855
Lee Seung Yun <ileilliat@gmail.com>
2022-02-10 12:11:18 +0900

    [server] Add missing files

---
+++ web/data/dummyLecture.js
@@ -0,0 +1,50 @@
+export const dummyLecture = [
+    {
+        "title": "컴맹을 위한 강의다!!",
+        "teacher": "김유미",
+        "description": "도비는 자유에요 집에가고싶어요~~ 쫌 이따 튀어야지 왜냐면 도비는 자유니까",
+        "src": "/img/sample-banner.jpg"
+    },
+    {
+        "title": "컴맹을 위한 강의다!!",
+        "teacher": "김유미",
+        "description": "도비는 자유에요 집에가고싶어요~~ 쫌 이따 튀어야지 왜냐면 도비는 자유니까",
+        "src": "/img/sample-banner.jpg"
+    },
+    {
+        "title": "컴맹을 위한 강의다!!",
+        "teacher": "김유미",
+        "description": "도비는 자유에요 집에가고싶어요~~ 쫌 이따 튀어야지 왜냐면 도비는 자유니까",
+        "src": "/img/sample-banner.jpg"
+    },
+    {
+        "title": "컴맹을 위한 강의다!!",
+        "teacher": "김유미",
+        "description": "도비는 자유에요 집에가고싶어요~~ 쫌 이따 튀어야지 왜냐면 도비는 자유니까",
+        "src": "/img/sample-banner.jpg"
+    },
+    {
+        "title": "컴맹을 위한 강의다!!",
+        "teacher": "김유미",
+        "description": "도비는 자유에요 집에가고싶어요~~ 쫌 이따 튀어야지 왜냐면 도비는 자유니까",
+        "src": "/img/sample-banner.jpg"
+    },
+    {
+        "title": "컴맹을 위한 강의다!!",
+        "teacher": "김유미",
+        "description": "도비는 자유에요 집에가고싶어요~~ 쫌 이따 튀어야지 왜냐면 도비는 자유니까",
+        "src": "/img/sample-banner.jpg"
+    },
+    {
+        "title": "컴맹을 위한 강의다!!",
+        "teacher": "김유미",
+        "description": "도비는 자유에요 집에가고싶어요~~ 쫌 이따 튀어야지 왜냐면 도비는 자유니까",
+        "src": "/img/sample-banner.jpg"
+    },
+    {
+        "title": "컴맹을 위한 강의다!!",
+        "teacher": "김유미",
+        "description": "도비는 자유에요 집에가고싶어요~~ 쫌 이따 튀어야지 왜냐면 도비는 자유니까",
+        "src": "/img/sample-banner.jpg"
+    },
+]
