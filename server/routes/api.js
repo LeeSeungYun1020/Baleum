92fa0f47f9db12755ae9374e10a26648a840f5af
Lee Seung Yun <ileilliat@gmail.com>
2022-04-04 22:33:51 +0900

    [server] Add API table

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,27 +9,52 @@ router.route('/')
 
         const createApiHtml = async () => {
             let html = ""
+            let table = `<table>
+<tr>
+    <th>이름</th><th>설명</th><th>방법</th><th>주소</th><th>파라미터</th><th>반환값</th>
+</tr>
+`
             const parser = fs
                 .createReadStream(`data/api.csv`)
                 .pipe(parse.parse({
                     // CSV options
                 }))
+            let isFirst = true
             for await (const record of parser) {
-                html += `<div>
-    <h2>${record[0]}</h2>
-    <h3>${record[2]}</h3>
-    <p>${record[3]}: ${record[4]}</p>
-    <p>params: ${record[5]}</p>
-    <p>return: ${record[6]}</p>
-</div>
+                html += `
+                    <div>
+                        <h2>${record[0]}</h2>
+                        <h3>${record[2]}</h3>
+                        <p>${record[3]}: ${record[4]}</p>
+                        <p>params: ${record[5]}</p>
+                        <p>return: ${record[6]}</p>
+                    </div>
 `
+                if (isFirst) {
+                    table += `
+                    <tr class="first">
+                        <td>${record[0]}</td><td>${record[2]}</td><td>${record[3]}</td>
+                        <td>${record[4]}</td><td>${record[5]}</td><td>${record[6]}</td>
+                    </tr>`
+                    isFirst = false
+                } else {
+                    table += `
+                        <tr class="else">
+                            <td>${record[0]}</td><td>${record[2]}</td><td>${record[3]}</td>
+                            <td>${record[4]}</td><td>${record[5]}</td><td>${record[6]}</td>
+                        </tr>`
+                }
+
+
             }
-            return html
+            table += "</table>"
+            return [html, table]
         }
 
         (async () => {
             const html = await createApiHtml()
-            req.session.apiDesc = html
+            req.session.apiDesc = html[0]
+            req.session.apiTable = html[1]
             res.redirect("/")
         })()
     })
