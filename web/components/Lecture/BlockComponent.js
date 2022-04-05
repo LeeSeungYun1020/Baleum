be094708f8f90f7088e4b8796e867f1a76423d88
Lee Seung Yun <ileilliat@gmail.com>
2022-04-05 10:56:27 +0900

    [web] Update block tooltip

--- web/components/Lecture/BlockComponent.js
+++ web/components/Lecture/BlockComponent.js
@@ -1,19 +1,20 @@
 import styles from "../../styles/Block.module.scss"
 import React, {useState} from "react";
 import {BsArrowRight} from "react-icons/bs";
+import BlockToolTip from "./BlockToolTip"
 
 const BlockComponent = (content) => {
     const [hide, setHide] = useState(true);
+    const mainContents = <div className={styles.block}>
+        <p>{content.content.title}</p>
+        <div className={styles.blockComponent}><p>{content.content.type}</p><p>{content.content.score}</p></div>
+        <p>{content.content.date.split("T")[0].replaceAll('-', '/')}</p>
+    </div>
+    const desc = `${content.content.feedback}`
     return (
         <>
-        <div className={styles.block} onMouseEnter={() => setHide(false)} onMouseLeave={()=>setHide(true)}>
-            {/*{console.log(content)}*/}
-            <p>{content.content.title}</p>
-            <div className={styles.blockComponent}><p>{content.content.type}</p><p>{content.content.score}</p></div>
-            <p>{content.content.date.split("T")[0]}</p>
-            {!hide && <p className={styles.blockHiddenInfo}>{content.content.feedback}</p>}
-        </div>
-        <BsArrowRight color={"#ffffff"} style={{margin: "auto 20px auto 0"}}/>
+            <BlockToolTip main={mainContents} description={desc}/>
+            <BsArrowRight color={"#ffffff"} style={{margin: "auto 20px auto 0"}}/>
         </>
     )
 }
