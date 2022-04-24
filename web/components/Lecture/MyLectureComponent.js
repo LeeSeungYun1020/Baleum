baad015954aac25f4b0353ca2a4353ef1f0eb662
gimume <yum17201619@gmail.com>
2022-04-24 17:17:04 +0900

    modify main and sub color

--- web/components/Lecture/MyLectureComponent.js
+++ web/components/Lecture/MyLectureComponent.js
@@ -22,7 +22,7 @@ const MyLectureComponent = (lecture) => {
     }
     return (<div className={styles.myLectureComponent} onClick={onClick}>
         <div className={styles.myLectureInfoText}>
-            <QRCode value={`${CLIENT_URL}/certification/lecture/${lecture.lecture.id}/${id}`} onClick={onQRClick} size={100} style={{float: "right"}}/>
+            <QRCode value={`${CLIENT_URL}/certification/lecture/${lecture.lecture.id}/${id}`} onClick={onQRClick} size={100} style={{float: "right", marginLeft: "20px"}}/>
             {/*모두가 볼 수 있는 사용자의 이 과목 학습 인증서로 넘어감*/}
             <div><h1 className={styles.myLectureText}>강의명 </h1><h1>{lecture.lecture.name}</h1></div>
             <div><h2 className={styles.myLectureText}>강의자 </h2><h2>{lecture.lecture.teacher}</h2></div>
