c4bd7d1aebae51360e4ea53230e4e20c2d418d0a
gimume <yum17201619@gmail.com>
2022-05-08 17:40:38 +0900

    [web] add notice create button

--- web/pages/lecture/[id]/notice/index.js
+++ web/pages/lecture/[id]/notice/index.js
@@ -1,14 +1,16 @@
 import {useRouter} from "next/router";
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import axios from "axios";
 import LectureNav from "../../../../components/Lecture/LectureNav";
 import styles from "../../../../styles/Lecture.module.scss";
 import {ROUTE_NOTICE_ID, SERVER_URL} from "../../../../data/global";
 import LectureTopNav from "../../../../components/Lecture/LectureTopNav";
 import Link from "next/link";
+import {LoginContext} from "../../../_app";
 
 const notice = () => {
     const router = useRouter()
+    const {currentUserId} = useContext(LoginContext);
     const {id} = router.query ;// 경로 /lecture/[id]/notice/[noticeId]
     if (!id) {
         return <></>
@@ -17,6 +19,7 @@ const notice = () => {
     const [notice, setNotice] = useState();
     const [loading, setLoading] = useState(false); // 데이터 로딩
     const [noticeExist, setNoticeExist] = useState(false); // 공지 없으면 F
+    const [userId, setUserId] = useState();
     useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
@@ -24,12 +27,13 @@ const notice = () => {
                 const response1 = await axios.get(`${SERVER_URL}/class/info/${id}`,{withCredentials: true})
                 if(response1.data[0].result) {
                     setLecture(response1.data[0])
+                    setUserId(response1.data[0].userId)
                 }
                 else {
                     router.push('/')
                 }
                 const response2 = await axios.get(`${SERVER_URL}/class/notice/class/${id}`, {withCredentials: true});
-                console.log(response2);
+                // console.log(response2);
                 if(response2.data[0].result){
                     setNotice(response2.data);
                     setNoticeExist(true);
@@ -65,8 +69,8 @@ const notice = () => {
                                query: { id: id, noticeId: index+1}
                                    }}><a>{list.id} {list.title}</a></Link>)}</div> : <p className={styles.lectureNoNotice}>등록된 공지사항이 없습니다.</p>)
                        }
+                       {userId === currentUserId && <button><Link href={`/lecture/${id}/notice/write`}><a>공지사항 작성</a></Link></button>}
                    </div>
-                }
             </div>
         </>)
 }
